747f75c04d71039a7a2a116735736735f73c5782
Jonathan Amaya (TatanL10n) <jonathanmmendieta@gmail.com>
2025-03-21 18:27:34 -0500

    Se agrega info y se aplica borde a la card, version 2

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,13 +1,29 @@
 // NOTE Types
 import { AboutMeProps } from "@/types";
+// NOTE Components
+import Image from "./Image";
 
-export default function AboutMe({ aboutMe } : AboutMeProps) {
+export default function AboutMe({ aboutMe }: AboutMeProps) {
+
+  const classLinks = `w-full md:w-1/2 mx-auto flex items-center gap-[3%] flex justify-center`;
   return (
-    <div className=" w-[90%] mx-auto flex flex-col md:flex-row items-center mt-[10%]">
+    <>
+      <div className=" w-[90%] mx-auto flex flex-col md:flex-row items-center justify-center mt-[10%]">
         <h2 className="w-full md:w-1/2 grid place-content-center text-[6rem] font-extrabold text-center">
           {aboutMe.title.split(' ')[0]} <br /> <span className="text-[4.5rem]">{aboutMe.title.split(' ')[1]}</span>
         </h2>
         <p className="w-full md:w-1/2 text-center font-normal text-lg">{aboutMe.description}</p>
-    </div>
+      </div>
+      <div className="w-[90%] mx-auto flex flex-col md:flex-row items-center justify-around mt-[4%]">
+        <a className={classLinks} href="https://www.linkedin.com/in/jonathan-amaya-mendieta/" target="_blank">
+          <Image src='linkedin' customClass='w-[2.5rem]' />
+          <p>My LinkedIn Profile</p>
+        </a>
+        <a className={classLinks} href="https://github.com/TatanLion" target="_blank">
+          <Image src='github-2' customClass='w-[2.5rem]' />
+          <p>My GitHub Profile</p>
+        </a>
+      </div>
+    </>
   )
 }
--- components/CardProject.tsx
+++ components/CardProject.tsx
@@ -7,27 +7,31 @@ interface CardProjectProps {
 
 export default function CardProject({ project }: CardProjectProps) {
     return (
-        <div className="w-[90%] mx-auto p-4 rounded-md shadow-lg bg-[#18181B] mb-[4%]">
-            <h3 className="text-xl font-bold mb-[2%]">{project.title}</h3>
-            <div className="w-full h-auto rounded-lg overflow-hidden">
-                <Image src={project.img} customClass="w-[90%]" />
-            </div>
-            <div className="mt-[4%]">
-                <a className="flex items-center justify-start gap-2 mt-2" href={project.github} target='_blank'>
-                    <Image src={'github'} customClass="w-[2rem]" />
-                    <p>View code</p>
-                </a>
-                <p className="text-md mt-2">{project.description}</p>
-                <div className="flex items-center flex-wrap gap-2 mt-2">
-                    <span className='font-semibold text-md'>Made with: </span>
-                    {project.tecnhnologies.map((tech, index) => (
-                        <div key={index} className="flex items-center justify-start relative group">
-                            <Image src={tech.toLowerCase()} customClass="w-[1.8rem]" />
-                            <div className="absolute left-1/2 -translate-x-1/2 bottom-full mt-2 px-3 py-1 rounded bg-white text-black text-sm font-bold opacity-0 group-hover:opacity-100 transition-opacity">
-                                {tech}
+        <div
+            className="w-[90%] mx-auto p-[2px] rounded-md mb-[4%] bg-gradient-to-b from-[#41d1ff] to-[#9231be] shadow-lg"
+        >
+            <div className="bg-[#18181B] p-4 rounded-md w-full h-full">
+                <h3 className="text-xl font-bold mb-[2%]">{project.title}</h3>
+                <div className="w-full h-auto rounded-lg overflow-hidden">
+                    <Image src={project.img} customClass="w-[90%]" />
+                </div>
+                <div className="mt-[4%]">
+                    <a className="flex items-center justify-start gap-2 mt-2" href={project.github} target='_blank'>
+                        <Image src={'github'} customClass="w-[2rem]" />
+                        <p>View code</p>
+                    </a>
+                    <p className="text-md mt-2">{project.description}</p>
+                    <div className="flex items-center flex-wrap gap-2 mt-2">
+                        <span className='font-semibold text-md'>Made with: </span>
+                        {project.tecnhnologies.map((tech, index) => (
+                            <div key={index} className="flex items-center justify-start relative group">
+                                <Image src={tech.toLowerCase()} customClass="w-[1.8rem]" />
+                                <div className="absolute left-1/2 -translate-x-1/2 bottom-full mt-2 px-3 py-1 rounded bg-white text-black text-sm font-bold opacity-0 group-hover:opacity-100 transition-opacity">
+                                    {tech}
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
